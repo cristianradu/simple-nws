922c10633c1fc5b2250415ffdb2a31a33f3c3b6f
Cristian Radu <github@cristianradu.com>
2011-02-07 02:14:27 -0500

    added the bare base class for the library

---
+++ simple-nws/SimpleNWS.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHP library for accessing NOAA's National Weather Service
+ *
+ * @author Cristian Radu <code@cristianradu.com>
+ * @version 1.0
+ * @package SimpleNWS
+ */
+class SimpleNWS
+{
+    //
+}
+?>
