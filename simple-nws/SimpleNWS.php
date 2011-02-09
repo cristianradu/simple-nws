787f1c81060129a443518087b6242cf8760a67b0
Cristian Radu <code@cristianradu.com>
2011-02-08 22:43:12 -0500

    added definitions for public methods

--- simple-nws/SimpleNWS.php
+++ simple-nws/SimpleNWS.php
@@ -9,5 +9,38 @@
 class SimpleNWS
 {
     //
+
+    /**
+     * Returns current weather conditions
+     *
+     * @param float $lat Latitude
+     * @param float $long Longitude
+     */
+    public function getCurrentConditions($lat, $long)
+    {
+        //
+    }
+
+    /**
+     * Returns weather forecast for the day
+     *
+     * @param float $lat Latitude
+     * @param float $long Longitude
+     */
+    public function getForecastForToday($lat, $long)
+    {
+        //
+    }
+
+    /**
+     * Returns weather forecast for the week
+     *
+     * @param float $lat Latitude
+     * @param float $long Longitude
+     */
+    public function getForecastForWeek($lat, $long)
+    {
+        //
+    }
 }
 ?>
